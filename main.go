5bcde073d7b56be3cdb9ed24147c18422e58854a
NeginSal <neginsaljooghi@gmail.com>
2025-08-06 17:51:30 +0330

    create basic Bubble Tea program with welcome message

--- main.go
+++ main.go
@@ -1,7 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+
+	tea "github.com/charmbracelet/bubbletea"
+)
+
+type model struct{}
+
+func (m model) Init() tea.Cmd {
+	return nil
+}
+
+func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	return m, nil
+}
+
+func (m model) View() string {
+	return "🌸 Welcome to Termoramic!"
+}
 
 func main() {
-	fmt.Println("Hello world")
+	p := tea.NewProgram(model{})
+	if err := p.Start(); err != nil {
+		fmt.Println("Error running program:", err)
+		os.Exit(1)
+	}
 }
